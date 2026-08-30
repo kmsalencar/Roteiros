d2b64ea7a99b7ca7521345a1ab22d5645c0eb0bf
kmsalencar <kmsalencar@gmail.com>
2026-08-30 14:33:54 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE = "roteiro-set2026-v4";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./roteiro.ics",
+  "./icon-180.png",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => Promise.allSettled(ASSETS.map(a => c.add(a))))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// Cache-first: abre offline. Em segundo plano busca versao nova.
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit => {
+      const net = fetch(e.request).then(res => {
+        if (res && res.ok) {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => hit || caches.match("./index.html"));
+      return hit || net;
+    })
+  );
+});
